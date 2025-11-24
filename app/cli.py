a3e56df30dc4454bb0fe4b79fc1fa2fde72ced18
Valtteri <mvirtai@proton.me>
2025-11-24 14:38:15 +0200

    Removed unnecissary lines of old code

--- app/cli.py
+++ app/cli.py
@@ -33,19 +33,3 @@ def cli(book: str, chapter: str, verses: str, output: str):
 
 if __name__ == "__main__":
     cli()
-
-
-
-
-# def get_ref_values() -> tuple[str, str, str]:
-#     book = input("Book: ").strip().lower()
-#     chapter = input("Chapter: ").strip()
-#     verses = input("Verse(s): ").strip()
-#     return book, chapter, verses
-
-
-# def display_verse(verse_data: dict) -> None:
-#     if verse_data:
-#         print(json.dumps(verse_data, indent=2))
-#     else:
-#         print("Failed to fetch verse. Check logs for details.")
