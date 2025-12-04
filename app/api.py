74c50d257cdad51e55d5793bfd898a42d507922d
Valtteri <mvirtai@proton.me>
2025-12-04 15:32:34 +0200

    Refactored the old fetch_verse_by_reference() function to handle use-mock-data, fetch verses, and fetch chapter cases in a single new function fetch_by_reference() that makes the API call.

--- app/api.py
+++ app/api.py
@@ -9,7 +9,10 @@ mock_data_path = Path(__file__).resolve().parent / "data" / "mock_data.json"
 BASE_URL = "http://bible-api.com"
 
 
-def fetch_verse_by_reference(book: str, chapter: str, verses: str, use_mock: bool = False) -> dict:
+def fetch_by_reference(book: str, chapter: str, verses: str | None, use_mock: bool = False) -> dict:
+    """Fetch a  verse, verses or a chapter from bible-api.com API"""
+
+    # Possibility to use mock data for testing purposes
     if use_mock:
         logger.info(f"Using mock data from path {mock_data_path}")
         
@@ -23,35 +26,38 @@ def fetch_verse_by_reference(book: str, chapter: str, verses: str, use_mock: boo
             logger.error(f"Invalid JSON in mock data: {exc}")
             return None
         return data
+    
+
+    # Fetch a single chapter
+    if not verses:
+        url = f"{BASE_URL}/{book}+{chapter}"
+        logger.info(f"Fetching a single chapter from path: {url}")
+    # Fetch a single verse or multiple verses
     else:
         url = f"{BASE_URL}/{book}+{chapter}:{verses}"
-        logger.info(f"Fetching data from {url}")
-        
-        try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status() # Heittää HTTPError, jos status code ei ole 2xx
+        logger.info(f"Fetching a single verse or multiple verses from path: {url}")
 
-            data = response.json()
-            logger.info(f"Succesfully fetched verse: {data.get('reference', 'unknown')}")
-            return data
+    # API call
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
 
-        except requests.exceptions.Timeout:
-            logger.error(f"Request to {url} timed out")
-            return None
-        except requests.exceptions.ConnectionError:
-            logger.error(f"Connection error when fetching {url}")
-            return None
-        except requests.exceptions.HTTPError as e:
-            logger.error(f"HTTP error {e.response.status_code}: {e.response.text}")
-            return None
-        except requests.exceptions.RequestException as e:
-            logger.error(f"Request failed: {e}")
-            return None
-        except json.JSONDecodeError:
-            logger.error(f"Invalid JSON response from {url}")
-            return None
+        data = response.json()
+        logger.info(f"Response status: {response.status_code}")
+        return data
 
-
-if __name__ == "__main__":
-    
-    fetch_verse_by_reference(book="John", chapter="3", verses="16")
+    except requests.exceptions.Timeout:
+        logger.error(f"Request to {url} timed out")
+        return None
+    except requests.exceptions.ConnectionError:
+        logger.error(f"Connection error when fetching {url}")
+        return None
+    except requests.exceptions.HTTPError as e:
+        logger.error(f"HTTP error {e.response.status_code}: {e.response.text}")
+        return None
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Request failed: {e}")
+        return None
+    except json.JSONDecodeError:
+        logger.error(f"Invalid JSON response from {url}")
+        return None
